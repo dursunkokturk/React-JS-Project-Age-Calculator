a380327d11a3fbd313f2920c2e155207048b8770
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-20 16:32:50 +0300

    src klasoru icindeki app.jsx component'inda tasarim kodlandi, index.html dosyasinda font eklendi, app.css dosyasinda css duzenlemeleri mobil gorunum icin yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import Bottom from "./assets/img/bottom.png"
 import './App.css'
 
 export default function App() {
   return (
     <>
+      <div className="container">
+        <div className="birthday">
+          <div className="day">
+            <p>GÜN</p>
+            <input type="text" />
+          </div>
+          <div className="month">
+            <p>AY</p>
+            <input type="text" />
+          </div>
+          <div className="year">
+            <p>YIL</p>
+            <input type="text" />
+          </div>
+        </div>
+        <img src={Bottom} className="bottom" alt="" />
+        <div className="results">
+          <div className="years">
+            <h1><span>--</span> Yıl</h1>
+          </div>
+          <div className="months">
+            <h1><span>--</span> Ay</h1>
+          </div>
+          <div className="day">
+            <h1><span>--</span> Gün</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
